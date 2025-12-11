31a190a4696f8096eefd3a57e152ae7ce3c21b9b
sidhubad143 <lakhwinder4332@gmail.com>
2025-12-11 16:12:58 +0530

    Update __init__.py

--- BADMUSIC/utils/__init__.py
+++ BADMUSIC/utils/__init__.py
@@ -9,3 +9,29 @@ from .inline import *
 from .pastebin import *
 from .sks import *
 from .sys import *
+
+
+BadMunda = [
+    "Bad1",
+    "Bad2",
+    "Bad3",
+    "Bad4",
+    "Bad5",
+    "Bad6",
+    "Bad7",
+    "Bad8",
+    "Bad9",
+    "Bad10",
+    "Bad11",
+    "Bad12",
+    "Bad13",
+    "Bad14",
+    "Bad15",
+    "Bad16",
+    "Bad17",
+    "Bad18",
+    "Bad19",
+    "Bad20",
+    "Bad21",
+    "Bad22",
+]
